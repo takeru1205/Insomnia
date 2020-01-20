640bdcbfc83ef2c9b13359ea6a280a493aee19a2
KONPEITO1205 <b1016036@fun.ac.jp>
2020-01-21 00:24:57 +0900

    [update] param bigger

--- TD3/models.py
+++ TD3/models.py
@@ -6,33 +6,29 @@ import torch.nn.functional as F
 class Actor(nn.Module):
     def __init__(self):
         super(Actor, self).__init__()
-        self.fc1 = nn.Linear(3, 12)
-        self.fc2 = nn.Linear(12, 12)
-        self.fc3 = nn.Linear(12, 12)
-        self.fc4 = nn.Linear(12, 1)
+        self.fc1 = nn.Linear(3, 400)
+        self.fc2 = nn.Linear(400, 300)
+        self.fc3 = nn.Linear(300, 1)
 
     def forward(self, obs):
         x = F.relu(self.fc1(obs))
         x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = self.fc4(x)
+        x = self.fc3(x)
         return x
 
 
 class Critic(nn.Module):
     def __init__(self):
         super(Critic, self).__init__()
-        self.fc1 = nn.Linear(3+1, 16)
-        self.fc2 = nn.Linear(16, 32)
-        self.fc3 = nn.Linear(32, 32)
-        self.fc4 = nn.Linear(32, 1)
+        self.fc1 = nn.Linear(3+1, 400)
+        self.fc2 = nn.Linear(300, 300)
+        self.fc3 = nn.Linear(300, 1)
 
     def forward(self, action, obs):
         x = torch.cat([obs, action], dim=1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = self.fc4(x)
+        x = self.fc3(x)
         return x
 
 
