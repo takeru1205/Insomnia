66dc75ae75d96c9c9acf740058d75f2b09fef530
KONPEITO1205 <b1016036@fun.ac.jp>
2020-01-23 00:50:52 +0900

    [fix] shallow network

--- CONV_TD3/models.py
+++ CONV_TD3/models.py
@@ -4,101 +4,72 @@ import torch.nn.functional as F
 
 
 class Actor(nn.Module):
-    def __init__(self, input_channel, action_dim):
+    def __init__(self, state_dim, action_dim):
         super(Actor, self).__init__()
-        self.conv1 = nn.Conv2d(input_channel, 32, kernel_size=4, stride=2)
-        #self.bn1 = nn.BatchNorm2d(32)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
-        #self.bn2 = nn.BatchNorm2d(64)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=2, stride=1)
-        #self.bn3 = nn.BatchNorm2d(64)
+        self.conv1 = nn.Conv2d(state_dim[0], 16, kernel_size=5, stride=2)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
+        self.bn3 = nn.BatchNorm2d(32)
 
-        self.fc_input = 10816
-        self.fc1 = nn.Linear(self.fc_input, 800)
-        #self.bn4 = nn.BatchNorm1d(800)
-        self.fc2 = nn.Linear(800, 400)
-        #self.bn5 = nn.BatchNorm1d(400)
-        self.fc3 = nn.Linear(400, action_dim)
+        def conv2d_size_out(size, kernel_size = 5, stride = 2):
+            return (size - (kernel_size - 1) - 1) // stride  + 1
+        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(state_dim[1])))
+        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(state_dim[2])))
+        linear_input_size = convw * convh * 32
+        self.out = nn.Linear(linear_input_size, action_dim)
 
     def forward(self, obs):
-        #x = F.relu(self.bn1(self.conv1(obs)))
-        #x = F.relu(self.bn2(self.conv2(x)))
-        #x = F.relu(self.bn3(self.conv3(x)))
-        x = F.relu(self.conv1(obs))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = x.view(-1, self.fc_input)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        #x = F.relu(self.bn4(self.fc1(x)))
-        #x = F.relu(self.bn5(self.fc2(x)))
-        x = torch.tanh(self.fc3(x))
+        x = F.relu(self.bn1(self.conv1(obs)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = x.view(x.size(0), -1)
+        x = torch.tanh(self.out(x))
         return x
 
 
 class Critic(nn.Module):
-    def __init__(self, input_channel, action_dim):
+    def __init__(self, state_dim, action_dim):
         super(Critic, self).__init__()
-        self.conv1 = nn.Conv2d(input_channel, 32, kernel_size=4, stride=2)
-        #self.bn1 = nn.BatchNorm2d(32)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
-        #self.bn2 = nn.BatchNorm2d(64)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=2, stride=1)
-        #self.bn3 = nn.BatchNorm2d(64)
+        self.conv1 = nn.Conv2d(state_dim[0], 16, kernel_size=5, stride=2)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
+        self.bn3 = nn.BatchNorm2d(32)
 
-        # convの後に全結合層を挟んでからconcatするか、挟まずにconcatするか
+        def conv2d_size_out(size, kernel_size = 5, stride = 2):
+            return (size - (kernel_size - 1) - 1) // stride  + 1
+        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(state_dim[1])))
+        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(state_dim[2])))
+        linear_input_size = convw * convh * 32
+        self.out1 = nn.Linear(linear_input_size + action_dim, action_dim)
 
-        self.fc_input = 10816
-        self.fc1 = nn.Linear(self.fc_input + action_dim, 800)
-        #self.bn4 = nn.BatchNorm1d(800)
-        self.fc2 = nn.Linear(800, 400)
-        #self.bn5 = nn.BatchNorm1d(400)
-        self.fc3 = nn.Linear(400, action_dim)
-
-        self.fc4 = nn.Linear(self.fc_input+action_dim, 800)
-        #self.bn6 = nn.BatchNorm1d(800)
-        self.fc5 = nn.Linear(800, 400)
-        #self.bn7 = nn.BatchNorm1d(400)
-        self.fc6 = nn.Linear(400, action_dim)
+        self.out2 = nn.Linear(linear_input_size + action_dim, action_dim)
 
     def forward(self, obs, action):
-        #x = F.relu(self.bn1(self.conv1(obs)))
-        #x = F.relu(self.bn2(self.conv2(x)))
-        #x = F.relu(self.bn3(self.conv3(x)))
-        x = F.relu(self.conv1(obs))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
+        x = F.relu(self.bn1(self.conv1(obs)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
 
-        x = x.view(-1, self.fc_input)
+        x = x.view(x.size(0), -1)
         x = torch.cat([x, action], dim=1)
-        x1 = F.relu(self.fc1(x))
-        x1 = F.relu(self.fc2(x1))
-        #x1 = F.relu(self.bn4(self.fc1(x)))
-        #x1 = F.relu(self.bn5(self.fc2(x1)))
-        x1 = self.fc3(x1)
 
-        x2 = F.relu(self.fc4(x))
-        x2 = F.relu(self.fc5(x2))
-        #x2 = F.relu(self.bn6(self.fc4(x)))
-        #x2 = F.relu(self.bn7(self.fc5(x2)))
-        x2 = self.fc6(x2)
+        x1 = self.out1(x)
+
+        x2 = self.out2(x)
 
         return x1, x2
 
     def q1(self, obs, action):
-        #x = F.relu(self.bn1(self.conv1(obs)))
-        #x = F.relu(self.bn2(self.conv2(x)))
-        #x = F.relu(self.bn3(self.conv3(x)))
-        x = F.relu(self.conv1(obs))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
+        x = F.relu(self.bn1(self.conv1(obs)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
 
-        x = x.view(-1, self.fc_input)
+        x = x.view(x.size(0), -1)
         x = torch.cat([x, action], dim=1)
-        x1 = F.relu(self.fc1(x))
-        x1 = F.relu(self.fc2(x1))
-        #x1 = F.relu(self.bn4(self.fc1(x)))
-        #x1 = F.relu(self.bn5(self.fc2(x1)))
-        x1 = self.fc3(x1)
+
+        x1 = self.out1(x)
 
         return x1
